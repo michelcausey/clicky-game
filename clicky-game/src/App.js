587c76ce2b2ef2050c02e4d637a9fc7514c821bd
MiChel Causey <michel.causey@gmail.com>
2019-01-18 13:34:26 -0500

    shuffle function / additional styling / added score function / added reset alert

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import clicks from "./clicks.json";
 import ImgCard from "./components/ImgCard";
 import Wrapper from "./components/Wrapper";
-import Score from "./components/Score"
+import _ from 'lodash';
 
 const Styling = {
-  background: "#defcec",
-  color: "black",
+  background: "#ff2400",
+  color: "#e2bf0b",
   textAlign: "center"
 };
 
@@ -16,21 +16,73 @@ class App extends Component {
     count: 0
   };
 
+  cardIncrement = id => {
+    // We always use the setState method to update a component's state
+    let clickedCard = false;
+    this.setState({
+      count: this.state.count + 1
+    });
+    this.setState({
+      clicks: this.state.clicks.map((god) => {
+  
+        if (god.id === id) {
+          if (god.clicked === true) {
+            clickedCard = true
+          }
+          god.clicked = true;
+        }
+        return god;
+      })
+    });
+    if (clickedCard === true) {
+      this.reset();
+    }
+    this.shuffle();
+  };
+
+  reset = () => {
+
+    alert("Try Again!")
+
+    this.setState({
+      clicks: this.state.clicks.map((god) => {
+        god.clicked = false;
+        return god;
+      }),
+      count: 0 });
+  };
+
+  shuffle = () => {
+    let godarray = this.state.clicks;
+    let shufflegods = _.shuffle(godarray);
+    this.setState({ clicks: shufflegods});
+  }
+
+
   render() {
     return (
       <Wrapper>
         <div className="App">
-          <div className="jumbotron jumbotron-fluid" style={Styling}>
+          <div className="jumbotron jumbotron-fluid" style={Styling}> 
             <div className="container">
-              <h1 className="display-4">Clicky Game</h1>
-              <p className="lead">only click each image once!</p>
+              <h1 className="display-4"><b>Clicky Game</b></h1>
+              <p className="lead"><i>can you click each image once?</i></p>
+              <hr></hr>
+              <p><h2>Score: {this.state.count} out of 12</h2></p>
             </div>
-            <Score />
           </div>
         </div>
 
         {this.state.clicks.map(god => (
-          <ImgCard id={god.id} image={god.image} />
+          <ImgCard 
+            id={god.id} 
+            image={god.image}
+            cardIncrement = {this.cardIncrement}
+
+          
+          
+          
+          />
         ))}
       </Wrapper>
     );
--- clicky-game/src/components/ImgCard/index.js
+++ clicky-game/src/components/ImgCard/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function ImgCard(props) {
   return (
     <div className="content">
-    <div className="card">
+    <div className="card" onClick={()=>props.cardIncrement(props.id)}>
       <div className="img-container">
         <img className="img-size" alt={props.name} src={props.image} />
       </div>
