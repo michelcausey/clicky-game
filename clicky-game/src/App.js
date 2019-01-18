e55e32249be710992d1b55ff278d5ccb0e6e843e
MiChel Causey <michel.causey@gmail.com>
2019-01-18 14:11:54 -0500

    update nicer images

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import clicks from "./clicks.json";
 import ImgCard from "./components/ImgCard";
 import Wrapper from "./components/Wrapper";
-import _ from 'lodash';
+import _ from "lodash";
 
 const Styling = {
-  background: "#ff2400",
-  color: "#e2bf0b",
-  textAlign: "center",
+  jumbo: {
+    background: "#ff2400",
+    color: "#e2bf0b",
+    textAlign: "center"
+  }
 };
 
 class App extends Component {
@@ -23,11 +25,10 @@ class App extends Component {
       count: this.state.count + 1
     });
     this.setState({
-      clicks: this.state.clicks.map((god) => {
-  
+      clicks: this.state.clicks.map(god => {
         if (god.id === id) {
           if (god.clicked === true) {
-            clickedCard = true
+            clickedCard = true;
           }
           god.clicked = true;
         }
@@ -41,50 +42,51 @@ class App extends Component {
   };
 
   reset = () => {
-
-    alert("Try Again!")
+    alert("Try Again!");
 
     this.setState({
-      clicks: this.state.clicks.map((god) => {
+      clicks: this.state.clicks.map(god => {
         god.clicked = false;
         return god;
       }),
-      count: 0 });
+      count: 0
+    });
   };
 
   shuffle = () => {
     let godarray = this.state.clicks;
     let shufflegods = _.shuffle(godarray);
-    this.setState({ clicks: shufflegods});
-  }
-
+    this.setState({ clicks: shufflegods });
+  };
 
   render() {
     return (
-      <Wrapper>
-        <div className="App">
-          <div className="jumbotron jumbotron-fluid" style={Styling}> 
-            <div className="container">
-              <h1 className="display-4"><b>Clicky Game</b></h1>
-              <p className="lead"><i>can you click each image once?</i></p>
-              <hr></hr>
-              <p><h2>Score: {this.state.count} out of 12</h2></p>
+        <Wrapper>
+          <div className="App">
+            <div className="jumbotron jumbotron-fluid" style={Styling.jumbo}>
+              <div className="container">
+                <h1 className="display-4">
+                  <b>Clicky Game</b>
+                </h1>
+                <p className="lead">
+                  <i>can you click each image once?</i>
+                </p>
+                <hr />
+                <p>
+                  <h2>Score: {this.state.count} out of 12</h2>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        {this.state.clicks.map(god => (
-          <ImgCard 
-            id={god.id} 
-            image={god.image}
-            cardIncrement = {this.cardIncrement}
-
-          
-          
-          
-          />
-        ))}
-      </Wrapper>
+          {this.state.clicks.map(god => (
+            <ImgCard
+              id={god.id}
+              image={god.image}
+              cardIncrement={this.cardIncrement}
+            />
+          ))}
+        </Wrapper>
     );
   }
 }
