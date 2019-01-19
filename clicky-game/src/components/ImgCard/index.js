54ac0e6c8d218d374c213c6c76e7a53236b15460
MiChel Causey <michel.causey@gmail.com>
2019-01-18 19:18:47 -0500

    styling again

--- clicky-game/src/components/ImgCard/index.js
+++ clicky-game/src/components/ImgCard/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function ImgCard(props) {
   return (
-    <div className="content">
+    <div className="content col-sm">
     <div className="card" onClick={()=>props.cardIncrement(props.id)}>
       <div className="img-container">
         <img className="img-size" alt={props.name} src={props.image} />
